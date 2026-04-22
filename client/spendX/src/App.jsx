ef0954b89511e13bde78910a1e2f9f7b78a0af9d
Ajin <ajinsanthosh555@gmail.com>
2026-04-22 23:21:31 +0530

    changes file struture

--- client/spendX/src/App.jsx
+++ client/spendX/src/App.jsx
@@ -1,17 +1,13 @@
 import React from 'react'
-import LoginPage from './auth/LoginPage'
-import RegisterPage from './auth/RegisterPage'
-import OtpVerifyPage from './auth/OtpVerifyPage'
-import EmailVerifyPage from './auth/EmailVerifyPage'
-import ForgetPassword from './auth/ForgetPassword'
+import LoginPage from './Features/auth/LoginPage'
+import RegisterPage from './Features/auth/RegisterPage'
+import ForgetPassword from './Features/auth/ForgetPassword'
 export default function App() {
   return (
     <div>
 
        {/* <LoginPage /> */}
        {/* <RegisterPage /> */}
-       {/* <OtpVerifyPage /> */}
-       {/* <EmailVerifyPage /> */}
        <ForgetPassword />
     </div>
   )
